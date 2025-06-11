5819248dda318454b5ffce5a4287fad1b965b6b5
DiegoInDaHu <dalvarezb99@gmail.com>
2025-06-12 01:04:02 +0200

    Improve update command and VLAN detection

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ set -e
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip python3-tk nmap arp-scan
 
-pip3 install --upgrade netifaces psutil scapy python-nmap
+pip3 install --upgrade netifaces psutil scapy python-nmap pyroute2
